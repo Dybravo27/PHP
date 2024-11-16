011612f6ccfd92b590fbddbadbe01b762266a3fc
Dylan Bravo <dylanslebyng@gmail.com>
2024-11-16 11:45:55 -0500

    conexion php a mysql (conexion de bases de datos)

--- crud/index.php
+++ crud/index.php
@@ -18,3 +18,21 @@ $ciudades = $bandera->fetchAll();
 echo "<pre>";
 print_r($ciudades);
 echo "</pre>";
+
+?>
+<form action="" method="post">
+    <div>
+        <label for="id_ciudad">CIUDAD</label>
+        <select name="id_ciudad" id="id_ciudad">
+            <?php
+            foreach ($ciudades as $key => $value) {
+            ?>
+            <option id="<?=$value['id_ciudad']?>">
+                <?= $value['nombre_ciudad'] ?>
+            </option>
+            <?php
+            }
+            ?>
+        </select>
+    </div>
+</form>
